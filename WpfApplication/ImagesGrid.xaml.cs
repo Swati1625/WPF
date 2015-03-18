7702ecef7b7ba6212f1f736fb894b43a72d65271
Swati Gupta <Swati.016gupta@hotmail.com>
2015-03-18 12:58:32 +0530

    First Commit

---
+++ WpfApplication/ImagesGrid.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication
+{
+    /// <summary>
+    /// Interaction logic for ImagesGrid.xaml
+    /// </summary>
+    public partial class ImagesGrid : Window
+    {
+        public ImagesGrid()
+        {
+            InitializeComponent();
+            BindImages();
+        }
+        private void BindImages()
+        {
+            List<Image> imgList = new List<Image>();
+            List<string> lstFileNames = new List<string>(System.IO.Directory.EnumerateFiles("D:\\Project\\WpfApplication\\WpfApplication\\Images", "*.jpg"));
+            foreach (string fileName in lstFileNames)
+            {
+                var imgTemp = new Image();
+                imgTemp.Source = new BitmapImage(new Uri(fileName));
+                imgTemp.Height = imgTemp.Width = 100;
+                imgList.Add(imgTemp);
+            }
+            ImageList.ItemsSource = imgList;
+        }
+    }
+}
---
+++ WpfApplication/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Data;
+
+namespace WpfApplication
+{
+
+    public partial class MainWindow : Window
+    {
+        static SQLiteConnection dbConnection = new SQLiteConnection("Data Source=Users.sqlite;Version=3;");
+        public MainWindow()
+        {
+            //CreateDB();
+            InitializeComponent();
+            GetUsers();
+        }
+        private void CreateDB()
+        {
+            SQLiteConnection.CreateFile("Users.sqlite");
+            dbConnection.Open();
+            string sql = "create table Users ( FirstName varchar(50), LastName varchar(50))";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            dbConnection.Close();
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            dbConnection.Open();
+            string sql = "insert into Users (FirstName, LastName) values ('" + firstName.Text + "','" + lastName.Text + "' )";
+            SQLiteCommand command = new SQLiteCommand(sql, dbConnection);
+            command.ExecuteNonQuery();
+            dbConnection.Close();
+            Reset();
+            GetUsers();
+        }
+        private void Reset()
+        {
+            firstName.Text = "";
+            lastName.Text = "";
+        }
+        private void GetUsers()
+        {
+            dbConnection.Open();
+            string sql = "select * from users";
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, dbConnection);
+            DataTable dtUser = new DataTable();
+            adapter.Fill(dtUser);
+            var userList = dtUser.AsEnumerable().Select(r => new Users()
+            {
+                FirstName = (string)r["FirstName"],
+                LastName = (string)r["LastName"]
+            }
+            ).ToList();
+            UsersGrid.ItemsSource = userList;
+            dbConnection.Close();
+
+           
+        }
+
+       
+        
+        private class Users
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+      
+    }
+}
